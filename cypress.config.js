af8bca4446a2c9eaf8c039f8b59244f7f8364cf9
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-07-04 22:40:44 +0300

    changed test

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    watchForFileChanges: false,
+    setupNodeEvents(on, config) {},
   },
 });
